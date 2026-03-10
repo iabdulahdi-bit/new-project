d6ce3d90140f527c5762aef133affb59e5f9a08b
iabdulahdi-bit <iabdulahdi@student.refactory.academy>
2026-03-10 14:00:53 +0300

    update sales page

--- modules/Customer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CustomerSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
-  address: String,
-});
-
-module.exports = mongoose.model("Customer", CustomerSchema);
